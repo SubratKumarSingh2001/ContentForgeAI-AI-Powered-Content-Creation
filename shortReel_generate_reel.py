751685a6bcabff74649245dcaa33584d3fb370bd
Subrat Kumar Singh <subratsingh2001@gmail.com>
2026-05-07 19:01:12 +0530

    Modified the text_to_speech() method

--- shortReel_generate_reel.py
+++ shortReel_generate_reel.py
@@ -1,48 +1,37 @@
 from flask import current_app
-from elevenlabs import VoiceSettings
-from elevenlabs.client import ElevenLabs
+from openai import OpenAI
 from dotenv import load_dotenv
 import os
 import subprocess
 
 load_dotenv()
 
-ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
-client = ElevenLabs(
-    api_key=ELEVENLABS_API_KEY,
-)
+key = os.getenv("API_KEY")
+client = OpenAI(api_key=key)
 
 def text_to_speech_file(text: str, folder: str) -> str:
-    # Calling the text_to_speech conversion API with detailed parameters
-    response = client.text_to_speech.convert(
-        voice_id="pNInz6obpgDQGcFmaJgB", # Adam pre-made voice
-        output_format="mp3_22050_32",
-        text=text,
-        model_id="eleven_flash_v2_5", # use the flash model for low latency
-        # Optional voice settings that allow you to customize the output
-        voice_settings=VoiceSettings(
-            stability=0.0,
-            similarity_boost=1.0,
-            style=0.0,
-            use_speaker_boost=True,
-            speed=1.0,
-        ),
-    )
-    # Generating a unique file name for the output MP3 file
+      # Ensure folder exists
+    os.makedirs(folder, exist_ok=True)
+
     save_file_path = os.path.join(folder, "audio.mp3")
 
-    # Writing the audio to a file
-    with open(save_file_path, "wb") as f:
-        for chunk in response:
-            if chunk:
-                f.write(chunk)
+    with client.audio.speech.with_streaming_response.create(
+        model="gpt-4o-mini-tts",
+        voice="nova",
+        input=text
+    ) as response:
 
-    print(f"{save_file_path}: A new audio file was saved successfully!")
+        response.stream_to_file(save_file_path)
+
+    print(f"{save_file_path}: Audio file generated successfully!")
 
 
 def reel_generate_ffmpeg(folder, rec_id) :
-    if not(os.path.exists(os.path.join('static', 'reels', rec_id))) :
-            os.mkdir(os.path.join('static', 'reels', rec_id))
+    # Ensure reel output folder exists
+    os.makedirs(
+        os.path.join('static', 'reels', rec_id),
+        exist_ok=True
+    )
 
     print("FOLDER:", folder)
     print("INPUT:", f"{folder}/input.txt")
